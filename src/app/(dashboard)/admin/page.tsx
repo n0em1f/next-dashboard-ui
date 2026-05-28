e84bce9550e2af1ec6bfd7d901d778c052379725
Noemi <noemi.feceu@gmail.com>
2026-05-28 19:43:14 +0300

    modificare package.json

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -5,6 +5,7 @@ import EventCalendarContainer from '@/components/EventCalendarContainer';
 import FinanceChart from '@/components/FinanceChart';
 import UserCard from '@/components/UserCard';
 export const dynamic = 'force-dynamic';
+
 const AdminPage = ({
   searchParams,
 }: {
