5400c141f767a413b0014072afe37ea770ec5f34
Noemi <noemi.feceu@gmail.com>
2026-05-28 18:18:19 +0300

    Adaugare force-dynamic in admin page

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -4,7 +4,7 @@ import CountChartContainer from '@/components/CountChartContainer';
 import EventCalendarContainer from '@/components/EventCalendarContainer';
 import FinanceChart from '@/components/FinanceChart';
 import UserCard from '@/components/UserCard';
-
+export const dynamic = 'force-dynamic';
 const AdminPage = ({
   searchParams,
 }: {
