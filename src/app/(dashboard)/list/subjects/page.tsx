028785bbc250e54184a8cd542721fe9775c58dda
Noemi <noemi.feceu@gmail.com>
2026-06-01 14:12:02 +0300

    fix subjects

--- src/app/(dashboard)/list/subjects/page.tsx
+++ src/app/(dashboard)/list/subjects/page.tsx
@@ -9,6 +9,8 @@ import { Prisma, Subject, Teacher } from '@prisma/client';
 import Image from 'next/image';
 import { auth } from '@clerk/nextjs/server';
 
+export const dynamic = 'force-dynamic';
+
 type SubjectList = Subject & { teachers: Teacher[] };
 
 const SubjectListPage = async ({
