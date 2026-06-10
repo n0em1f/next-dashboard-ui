f8866799e0177c13d84ccf9b72d180c2cf1987ed
Noemi <noemi.feceu@gmail.com>
2026-06-10 20:46:55 +0300

    salvare favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,10 +13,6 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Academos',
   description: 'Platforma de management academic',
-  icons: {
-    icon: '/logo.png',
-    shortcut: '/icon.png',
-  },
 };
 
 export default function RootLayout({
